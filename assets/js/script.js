d53a7f83c0fd7bdf86df3ed5fb1bf7742f70a072
narekmkrtchyan11 <narekmkrtchyan606@gmail.com>
2022-02-14 19:01:42 +0400

    finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,60 @@
+let postArray = [{
+    title: "Martiros Saryan",
+    imgURL: "/assets/images/Saryan.jpg",
+    like: "45"
+},
+{
+    title: "Pablo Ruiz Picasso",
+    imgURL: "/assets/images/Pablo.jfif",
+    like: "12"
+},
+{
+    title: "Hovhannes Aivazovski",
+    imgURL: "/assets/images/Ayvazovski.jpg",
+    like: "78"
+},
+{
+    title: "Leonardo da Vinci",
+    imgURL: "/assets/images/Monaliza.jpg",
+    like: "0"
+}]
+
+function addPost (title,imgURL,id,like){
+    return `<div id="post${id}" class="post">
+    <h2>${title}</h2>
+    <div class="arrowDiv">
+        <i  class="fas fa-arrow-up fa-2x arrowUp" ></i> </br>
+        <p class="bal">${like}</p>
+        <i  class="fas fa-arrow-down fa-2x arrowDown"></i>
+    </div>
+    <div class="postDiv">
+        <img src="${imgURL}"  alt="">
+        <div class="appearingDiv">
+            <button  class="deleteCommentDiv but ">Close </button>
+            <button  class="addCommentDiv but">Comment  </button>
+        </div>
+    </div>
+    <div class="mainContainer">
+    <div class="AddDiv1">
+        <form class="mui-form">
+                <textarea class="AddInput" type="text" maxlength="150"   placeholder="Comment" onfocus="this.placeholder = 'Please write your comment...'" onblur="this.placeholder = 'Comment'" ></textarea>
+                <button class="AddButton">Add</button>
+        </form>
+    </div>
+    </div>
+</div>`;
+}
+
+function setPost (){
+    let ourPosts = "";
+    for(let i = 0; i < postArray.length;i++){
+        ourPosts = ourPosts + addPost(postArray[i].title,postArray[i].imgURL,i + 1,postArray[i].like);
+    }
+    document.getElementById("content").innerHTML = ourPosts;
+}
+setPost();
 
 const posts = document.getElementsByClassName("post");
-
 for (const post of posts) {
 const addButton = post.getElementsByClassName("AddButton")[0];
 const mainContainer = post.getElementsByClassName("mainContainer")[0];
@@ -10,6 +64,8 @@ const bal = post.getElementsByClassName("bal")[0];
 
 addCommentDiv.addEventListener("click",function(){
     mainContainer.style.display = "block";
+    const commentArea = post.getElementsByClassName("AddInput")[0];
+commentArea.focus();
 })
 
 deleteCommentDiv.addEventListener("click",function(){
@@ -20,12 +76,9 @@ addButton.addEventListener("click",function(evt) {
     evt.preventDefault();
     const textArea1 = evt.target.previousElementSibling;
     const comment = textArea1.value;
-
-    
     if(comment !== ""){
         addCommentFunc(comment,textArea1);
     }
-    
 });
 
 function addCommentFunc (value,textArea1) {
